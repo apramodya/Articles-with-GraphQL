4a1cca970e8bbc02d968b6f1c002b448d7af2fb6
Pramodya <pramodya.emedia@gmail.com>
2021-05-18 18:49:16 +0530

    Initial Commit

---
+++ Articles with GraphQL/Articles_with_GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Articles_with_GraphQLApp.swift
+//  Articles with GraphQL
+//
+//  Created by Pramodya Abeysinghe on 2021-05-18.
+//
+
+import SwiftUI
+
+@main
+struct Articles_with_GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
